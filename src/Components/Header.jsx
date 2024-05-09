068b34869260ee000cda211654579453b61c13b3
Alex Mdz <ichialex5620mail.com>
2024-05-09 11:09:40 -0600

    Agregando el Select al header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,6 +6,7 @@ import Image from 'react-bootstrap/Image';
 import '../Styles/Header.css'
 import logo from '../assets/IconNav.svg'
 import car from "../assets/cart.svg";
+import Select from './Select';
 
 
 function Header() {
@@ -20,6 +21,53 @@ function Header() {
         <div id='navBarDiv'>
           <Nav className="me-auto">
               <NavLink to="/" className="btn btn-success">Inicio</NavLink>
+
+
+
+        <select className="btn btn-success">
+          <option disabled="true" selected>
+            <span className="">
+             Géneros
+            </span>
+          </option>
+          <Select />
+            <ul>
+              <li>
+                <NavLink to="/products/gadgets" className="dropdown-link">
+                  Gadgets
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/products/consolas" className="dropdown-link">
+                  Consolas
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/products/mobile" className="dropdown-link">
+                  Mobile
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/products/monitors" className="dropdown-link">
+                  Monitors
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/products/tablets" className="dropdown-link">
+                  Tablets
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/products/pc" className="dropdown-link">
+                  PC
+                </NavLink>
+              </li>
+            </ul>
+
+        </select>
+
+
+
               <NavLink to="/login" className="btn btn-success">Login/Registro</NavLink>
               <NavLink to="/contactanos" className="btn btn-success">Contactanos</NavLink>
               <NavLink to="/carrito" className="btn btn-success">
---
+++ src/Components/Select.jsx
@@ -0,0 +1,12 @@
+
+function Select() {
+  return (
+    <>
+        <option className="container-center">
+            Action
+        </option>
+    </>
+  )
+}
+
+export default Select
