295a2a725d7be74afc615d16e2d3c9ff3985336c
Alex Mdz <ichialex5620mail.com>
2024-05-12 10:53:05 -0600

    Modificacion al carrito y al selector de categorias

--- src/Components/Select.jsx
+++ src/Components/Select.jsx
@@ -1,12 +1,21 @@
+import Dropdown from 'react-bootstrap/Dropdown';
 
 function Select() {
   return (
-    <>
-        <option className="container-center">
-            Action
-        </option>
-    </>
-  )
+    <Dropdown>
+      <Dropdown.Toggle variant="success" id="dropdown-basic">
+        Géneros
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        {/* Aqui tiene que ir la logica para pasarle los generos, pero no se como hacerla */}
+        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+      </Dropdown.Menu>
+  </Dropdown>
+);
+
 }
 
 export default Select
--- src/Pages/Car.jsx
+++ src/Pages/Car.jsx
@@ -1,40 +1,57 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 import '../Styles/Car.css'
 
 function Car() {
   return (
     <>
-      <main>
-        <div className='bg-violet my-car'>
-          <div className='title'>
-            <h1>Mi carrito</h1>
-          </div>
-          <div className='data-game'>
-            <div>
-              <img src="" alt="Imagen del juego" />
-            </div>
-            <div>
-              <h3>Nombre del Juego</h3>
-            </div>
-            <div>
-              <button>Borrar</button>
-            </div>
-          </div>
-        </div>
-        <div className='bg-violet my-summary'>
-          <div>
-            <h1>Resumen</h1>
-          </div>
-          <div>
-            <button>Finalizar compra</button>
-          </div>
-          <div>
-            <p>Cantidad de Producto - Costo del prodcuto por cantidad</p>
-          </div>
-          <div>
-            <p>Total</p>
-          </div>
-        </div>
-      </main>
+      <Container className='mt-5'>
+        <Row className="justify-content-center">
+          <Col md="auto" className='bg-violet'>
+            <Row className='sticky-top'>
+              <h1 className='title'>Mi carrito</h1>
+            </Row>
+            <Row className='data-game'>
+              <Col>
+                <img src="" alt="Imagen del juego" />
+              </Col>
+              <Col md="auto">
+                <Row>
+                  <Col>
+                    <h3>Nombre del juego</h3>
+                  </Col>
+                  <Col className='d-flex justify-content-end'>
+                    <button className='btn btn-success'>Borrar</button>
+                  </Col>
+                </Row>
+                <Row xs="auto">
+                  <Col className='d-flex justify-content-end'>
+                    <Button variant="success" size="sm">-</Button>
+                    {'cantidad a comprar'}
+                    <Button variant="success" size="sm">+</Button>
+                  </Col>
+                  <Col className='d-flex justify-content-end'>
+                    <p>Costo unitario</p>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Col>
+          <Col xs lg="3" className='bg-violet '>
+            <Col>
+              <h1>Resumen</h1>
+            </Col>
+            <Col>
+              <button className='btn btn-success'>Finalizar compra</button>
+            </Col>
+            <Col>
+              <p>Total - Cantidad total del Costo</p>
+            </Col>
+          </Col>
+        </Row>
+      </Container>
     </>
   )
 }
